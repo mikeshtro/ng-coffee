0cc40e85716d3341175c76fdfe64afd8faa62a21
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2026-01-05 15:29:40 +0100

    Add common pipes step

---
+++ src/files/angular-basics-2025/11-common-pipes/src/app/app.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { CoffeePrice } from './coffee-price';
+import { CoffeeType } from './coffee-type';
+import { CoffeeOverview } from './pick-coffee/coffee-overview/coffee-overview';
+
+@Component({
+  selector: 'mcf-root',
+  imports: [CoffeeOverview],
+  template: `
+    <div class="total">Total price: {{ getTotalPrice() }}</div>
+    <div>
+      @for (coffee of coffees; track coffee.id) {
+        <mcf-coffee-overview
+          [price]="coffee.price"
+          [amount]="orderedCoffees.get(coffee.id)"
+          (amountChange)="orderCoffee($event, coffee.id)"
+        >
+          <span class="coffee">{{ coffee.id }}</span>
+        </mcf-coffee-overview>
+      }
+    </div>
+  `,
+  styles: `
+    .coffee {
+      color: #12ae12;
+    }
+
+    .total {
+      font-size: 2rem;
+      line-height: 1.75;
+    }
+  `,
+})
+export class App {
+  protected readonly coffees: CoffeePrice[] = [
+    { id: 'espresso', price: 12.43 },
+    { id: 'latte', price: 22.64 },
+    { id: 'cappuccino', price: 24.76 },
+  ];
+
+  protected orderedCoffees = new Map<CoffeeType, number>();
+
+  protected orderCoffee(amount: number, id: CoffeeType): void {
+    this.orderedCoffees.set(id, amount);
+  }
+
+  protected getTotalPrice(): number {
+    let totalPrice = 0;
+
+    for (const price of this.coffees) {
+      const amount = this.orderedCoffees.get(price.id) ?? 0;
+      totalPrice += amount * price.price;
+    }
+
+    return totalPrice;
+  }
+}
