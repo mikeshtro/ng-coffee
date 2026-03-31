05b6c311781e9085ac9ebe70f40cd0c3f5dc7c7b
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2026-03-31 15:37:24 +0200

    Fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig(({ mode }) => ({
       prerender: {
         routes: [],
       },
+      nitro: {
+        preset: 'vercel',
+      },
       content: {
         highlighter: 'prism',
         prismOptions: {
